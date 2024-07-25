ddbb352c08e4723eee1fbe5828462ec795518aa1
Caio1213 <caiofxpgm@gmail.com>
2024-07-25 18:08:21 -0300

    Edit JS, alteração checkbox, projeto final antes da integração banco de dados

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ function bilhete (){
     const nome = document.getElementById('txtNome').value;
     const email = document.getElementById('txtEmail').value;
     const telefone = document.getElementById('txtTelefone').value;
-    const presença = document.getElementById('checkY')
+    const presença = document.getElementById('checkY');
+    const nPresença = document.getElementById('checkN');
 
-    if(presença.checked){
-        alert('Presença confirmada')
-    }
-
-    if(nome !== '' && email !== '' && telefone !== '' && presença.checked){
-        alert('Dados salvos e presença confirmada')
+    if(presença.checked && nPresença.checked){
+        alert('Marque apenas uma opção no checkbox');
     }
     else{
-        alert('Certifique-se de que preencheu todos as caixas de textos')
+        if(nome !== '' && email !== '' && telefone !== '' && presença.checked){
+            alert('Dados salvos e presença confirmada');
+        }
+        else{
+            alert('Certifique-se de que preencheu todos as caixas de textos');
+        }
     }
 }
 
